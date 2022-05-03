9f33c81af67d1ed54cd21b29b9422e43a8c82dbd
facundotdf <104038338+facundotdf@users.noreply.github.com>
2022-05-03 18:18:53 -0300

    correccion correccion

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import "./styles.css";
+let niveldecombustibleactual: number = 0;
+let nivel_tanque_lleno: number = 40;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+while (niveldecombustibleactual < nivel_tanque_lleno) {
+  console.log("canrgando 10 litrso de conbustible");
+  niveldecombustibleactual = niveldecombustibleactual += 10;
+}
+console.log("tanque lleno");
